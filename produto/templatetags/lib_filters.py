79910877790a5c3fe5b4ffdf3357a2fd8b7fcb51
Gustavo Scheffer <gustavoascheffer@gmail.com>
2022-07-26 09:17:27 -0300

    5rd Added Product Details

--- produto/templatetags/lib_filters.py
+++ produto/templatetags/lib_filters.py
@@ -5,10 +5,6 @@ from uteis import formatar_preco
 register = Library()
 
 
-# @register.filter
-# def formata_preco(val):
-#     return f'R$ {val:,.2f}'.replace('.', ',')
-
 @register.filter
 def formata_preco(val):
     return formatar_preco.formata_preco(val)
